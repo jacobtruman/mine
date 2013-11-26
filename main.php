13e104e9795cf8d07614a39bec3e8ade81e565f5
Jacob Truman <jacob.truman@gmail.com>
2013-11-26 16:51:32 -0700

    Add picture rename script

---
+++ main.php
@@ -0,0 +1,55 @@
+#!/usr/bin/php
+<?
+ini_set('display_errors', 1);
+// Script to rename picture based on date taken value encoded into image
+
+$year = "2013";
+//reRunYear($year);
+
+$path = "/chell/d/Pictures/camera/".$year."/NEW";
+//$files = glob($path."/*.jpg");
+$files = glob($path."/*.JPG");
+//var_dump($files);
+foreach($files as $file)
+{
+	renameFile($file);
+}
+
+function renameFile($file)
+{
+	$exif = read_exif_data ($file);
+	$datetime = (isset($exif['DateTimeDigitized']) ? $exif['DateTimeDigitized'] : (isset($exif['DateTimeOriginal']) ? $exif['DateTimeOriginal'] : $exif['DateTime']));
+	if(!empty($datetime))
+	{
+		$ts = strtotime($datetime);
+		if(!empty($ts))
+		{
+			$new_file = getFilename($ts);
+			echo "Renaming file ".$file." to ".$new_file."\n";
+			copy($file, $new_file);
+			#rename($file, $new_file);
+		}
+	}
+}
+
+function getFilename($ts)
+{
+	// format yyyy-mm-dd_hh'mm'ss
+	$filename = date("Y-m-d_H'i's", $ts).".jpg";
+	$year = date("Y", $ts);
+	$month = date("M", $ts);
+	$path = $year."/".$month;
+	if(!is_dir($year))
+		mkdir($year, 0777);
+	if(!is_dir($path))
+		mkdir($path, 0777);
+	if(file_exists($path."/".$filename))
+	{
+		echo "file exists, trying again\n";
+		return getFilename(++$ts, $path);
+	}
+	else
+		return $path."/".$filename;
+}
+
+?>
