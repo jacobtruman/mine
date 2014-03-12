1aaa657fff9f7f341964063fff307ea34eb1c065
Jacob Truman <jacob.truman@gmail.com>
2014-03-12 07:55:14 -0600

    Update testOrganizePictures to use new Photo.class

--- testOrganizePictures.php
+++ testOrganizePictures.php
@@ -1,59 +1,23 @@
 #!/usr/bin/php
 <?
 ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once("Photo.class.php");
+
 // Script to rename picture based on date taken value encoded into image
+// TODO: need to add 12 hours to all NIKON D3200 images, then delete the source files to ensure no duplication
+// TODO: Move NEW directory outside the year directory
 
-$year = "2013";
+runProcess();
 
-$path = "/mine/backup/Pictures/camera/".$year."/Nov";
-$files = glob($path."/*.jpg");
-foreach($files as $file)
-{
-	renameFile($file);
-}
-
-function renameFile($file)
-{
-	$exif = read_exif_data ($file);
-	if($exif['Model'] === "NIKON D3200") {
-		var_dump($file);
-		var_dump($exif['Make']);
-		var_dump($exif['Model']);
-		$datetime = (isset($exif['DateTimeDigitized']) ? $exif['DateTimeDigitized'] : (isset($exif['DateTimeOriginal']) ? $exif['DateTimeOriginal'] : $exif['DateTime']));
-		var_dump($datetime);
-		exit;
-		if(!empty($datetime))
-		{
-			$ts = strtotime($datetime);
-			if(!empty($ts))
-			{
-				$new_file = getFilename($ts);
-				echo "Renaming file ".$file." to ".$new_file."\n";
-				#copy($file, $new_file);
-				#rename($file, $new_file);
-			}
-		}
+function runProcess() {
+	$path = "/mine/backup/Pictures/camera/2013/Nov";
+	$files = glob($path."/*.jpg");
+	foreach($files as $file) {
+		$photo = new Photo($path, $file);
+		$photo->renameFile();
 	}
-}
-
-function getFilename($ts)
-{
-	// format yyyy-mm-dd_hh'mm'ss
-	$filename = date("Y-m-d_H'i's", $ts).".jpg";
-	$year = date("Y", $ts);
-	$month = date("M", $ts);
-	$path = $year."/".$month;
-	if(!is_dir($year))
-		mkdir($year, 0777);
-	if(!is_dir($path))
-		mkdir($path, 0777);
-	if(file_exists($path."/".$filename))
-	{
-		echo "file exists, trying again\n";
-		return getFilename(++$ts, $path);
-	}
-	else
-		return $path."/".$filename;
 }
 
 ?>
