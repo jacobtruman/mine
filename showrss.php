b693539fbd7ccc31d1816ce75dc057123dc92d17
Jacob Truman <jacob.truman@gmail.com>
2014-09-09 13:56:42 -0600

    Account for no items being returned

--- showrss.php
+++ showrss.php
@@ -8,49 +8,51 @@ addLog("#### Starting process ####");
 runProcess();
 addLog("#### Ending process ####");
 
-function runProcess()
-{
+function runProcess() {
 	$rss = "http://showrss.info/rss.php?user_id=68354&hd=null&proper=null&magnets=false";
 
 	$contents = file_get_contents($rss);
 
-	if(!empty($contents))
-	{
+	if(!empty($contents)) {
 		$xml = new SimpleXMLElement($contents);
 
 		$json = json_encode($xml);
 		$item_array = json_decode($json, true);
 
-		$items = $item_array['channel']['item'];
+		$items = isset($item_array['channel']['item']) ? $item_array['channel']['item'] : array();
 
 		processItems($items);
 	}
 }
 
-function processItems($items)
-{
+function processItems($items) {
 	addLog(count($items)." to be checked");
-	foreach($items as $index=>$item)
-	{
-		$show = new TVShow($item['title']);
-		$url = $item['link'];
+	if(count($items) > 0) {
+		foreach($items as $index=>$item) {
+			$show = new TVShow($item['title']);
+			$url = $item['link'];
 
-		addLog($show->show.": ".$show->episode);
-		$filename = str_replace(" ", "_", $show->show." - ".$show->episode.".torrent");
+			addLog($show->show.": ".$show->episode);
+			$filename = str_replace(" ", "_", $show->show." - ".$show->episode.".torrent");
 
-		getTorrent($url, $filename);
+			getTorrent($url, $filename);
+		}
 	}
 }
 
-function getTorrent($url, $filename)
-{
+function getTorrent($url, $filename) {
 	$torrenttmp_dir = "/torrents/.torrentstmp";
 	$torrent_dir = "/torrents/.torrents";
 	chdir($torrenttmp_dir);
 
 	$full_filename = $torrenttmp_dir."/".$filename;
-	if(!file_exists($full_filename))
-	{
+	$full_filename_invalid = $torrent_dir."/".$filename.".invalid";
+	if(file_exists($full_filename_invalid)) {
+		echo "Invalid file found - deleting it: ".$full_filename_invalid."\n";
+		unlink($full_filename);
+		unlink($full_filename_invalid);
+	}
+	if(!file_exists($full_filename)) {
 		addLog("\tGetting file: ".$full_filename." from ".$url);
 		$fp = fopen($full_filename, 'w+');//This is the file where we save the information
 		$ch = curl_init($url);//Here is the file we are downloading
@@ -64,25 +66,20 @@ function getTorrent($url, $filename)
 		curl_close($ch);
 		fclose($fp);
 		copy($full_filename, $torrent_dir."/".$filename);
-	}
-	else
-	{
+	} else {
 		addLog("\tfile exists: ".$full_filename);
 		//getTorrentInfo($full_filename);
 	}
 	chdir(dirname(__FILE__));
 }
 
-function getTorrentInfo($torrent_file = "")
-{
-	if(!empty($torrent_file) && file_exists($torrent_file))
-	{
+function getTorrentInfo($torrent_file = "") {
+	if(!empty($torrent_file) && file_exists($torrent_file)) {
 		var_dump(`btshowmetainfo $torrent_file`);
 	}
 }
 
-function addLog($msg)
-{
+function addLog($msg) {
 	echo date("Y-m-d h:i:s")."\t".$msg."\n";
 }
 
