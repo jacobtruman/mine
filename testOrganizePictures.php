ac2274d13fe972d10384fe95ef6691ce3dd47608
Jacob Truman <jtruman@Jacobs-MBP.sfcn.org>
2016-02-15 22:14:09 -0700

    Enhance photo organization script

--- testOrganizePictures.php
+++ testOrganizePictures.php
@@ -5,25 +5,28 @@ error_reporting(E_ALL);
 
 require_once("Photo.class.php");
 
-// Script to rename picture based on date taken value encoded into image
-// TODO: need to add 12 hours to all NIKON D3200 images, then delete the source files to ensure no duplication
-// TODO: Move NEW directory outside the year directory
-
 runProcess();
 
 function runProcess() {
-	$args = getopt("s:d:e");
+	$args = getopt("s:d:ev");
 
 	$dry_run = true;
 	if(isset($args['e'])) {
 		$dry_run = false;
 	}
 
+	$verbose = false;
+	if(isset($args['v'])) {
+		$verbose = true;
+	}
+
 	$base_path = "/mine/Pictures/camera";
+	$dest_path = "/mine/Pictures2/camera";
 	$path = $base_path."/NEW";
 	$files = glob($path."/*.{jpg,JPG}", GLOB_BRACE);
 	foreach($files as $file) {
-		$photo = new Photo($path, $file, $base_path, $dry_run);
+		$photo = new Photo($path, $file, $base_path, $dry_run, $verbose);
+		$photo->setDestPath($dest_path);
 		$photo->renameFile();
 	}
 }
