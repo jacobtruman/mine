badb72b8f0c2214fa7c8104d00f885709983c064
Jacob Truman <jatruman@adobe.com>
2017-03-30 16:24:59 -0600

    Update ipMonitor

--- ipMonitor.php
+++ ipMonitor.php
@@ -1,5 +1,20 @@
 #!/usr/local/bin/php
 <?php
 
+$ip_file = "./.ip_address";
+
 $cmd = "dig +short myip.opendns.com @resolver1.opendns.com";
 
+if(file_exists($ip_file)) {
+	$current_ip = file_get_contents($ip_file);
+} else {
+	$current_ip = null;
+}
+
+exec($cmd, $ip);
+
+if($ip !== $current_ip) {
+	file_put_contents($ip, $ip_file);
+}
+
+unset($ip);
