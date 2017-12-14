b084493b3fe6ab5fb87aac86f02739935dabc29b
Jacob Truman <jatruman@adobe.com>
2017-12-14 16:57:15 -0700

    Don't try to get shows that are no longer accessible

--- getShows.php
+++ getShows.php
@@ -1,7 +1,7 @@
 <?php
 
 $cbs_shows_file = "./cbs_shows.json";
-getCBSShows($cbs_shows_file);
+//getCBSShows($cbs_shows_file);
 
 $nbc_shows_file = "./nbc_shows.json";
 getNBCShows($nbc_shows_file);
@@ -28,6 +28,8 @@ function getCBSShows($shows_file) {
 				foreach ($json['result']['data'] as $record) {
 					processUrl("{$base_url}{$record['url']}");
 				}
+
+				unlink($data_file);
 			}
 		}
 	}
@@ -39,7 +41,7 @@ function getNBCShows($shows_file) {
 		foreach ($shows_to_get as $show_info) {
 			$show_id = $show_info['show_id'];
 			$show_title = $show_info['show_title'];
-			$show_url = "https://api.nbc.com/v3.14/videos?fields%5Bvideos%5D=title%2Ctype%2Cavailable%2CseasonNumber%2CepisodeNumber%2Cexpiration%2Cpermalink%2CembedUrl&fields%5Bshows%5D=description%2Cname%2CshortDescription%2CshortTitle&fields%5Bimages%5D=derivatives%2Cpath%2Cwidth&fields%5Bseasons%5D=seasonNumber%2CcontestantTitle&fields%5BgenereticProperties%5D=showCollection.collections&include=show%2Cshow.season%2Cshow.iosProperties.compactImage%2Cshow.genereticProperties.showCollection.collections&derivatives=landscape.widescreen.size640.x1&filter%5Bshow%5D={$show_id}&filter%5Bavailable%5D%5Bvalue%5D=2017-12-14T12%3A30%3A00-05%3A00&filter%5Bavailable%5D%5Boperator%5D=%3C%3D&filter%5Btype%5D%5Bvalue%5D=Full%20Episode&filter%5Btype%5D%5Boperator%5D=%3D&sort=-airdate";
+			$show_url = "https://api.nbc.com/v3.14/videos?fields%5Bvideos%5D=title%2Ctype%2Cavailable%2CseasonNumber%2CepisodeNumber%2Cexpiration%2Centitlement%2CtveAuthWindow%2CnbcAuthWindow%2Cpermalink%2CembedUrl&fields%5Bshows%5D=description%2Cname%2CshortDescription%2CshortTitle&fields%5Bimages%5D=derivatives%2Cpath%2Cwidth&fields%5Bseasons%5D=seasonNumber%2CcontestantTitle&fields%5BgenereticProperties%5D=showCollection.collections&include=show%2Cshow.season%2Cshow.iosProperties.compactImage%2Cshow.genereticProperties.showCollection.collections&derivatives=landscape.widescreen.size640.x1&filter%5Bshow%5D={$show_id}&filter%5Bavailable%5D%5Bvalue%5D=2017-12-14T12%3A30%3A00-05%3A00&filter%5Bavailable%5D%5Boperator%5D=%3C%3D&filter%5Btype%5D%5Bvalue%5D=Full%20Episode&filter%5Btype%5D%5Boperator%5D=%3D&sort=-airdate";
 
 			$data_file = "./" . str_replace(" ", "_", strtolower($show_title)) . ".json";
 
@@ -50,18 +52,30 @@ function getNBCShows($shows_file) {
 			$now = time();
 			foreach($json['data'] as $record) {
 				$attributes = $record['attributes'];
-				$season_number = $attributes['seasonNumber'];
-				$episode_number = $attributes['episodeNumber'];
-				$expire_ts = strtotime($attributes['expiration']);
-				$season = str_pad($season_number, 2, "0", STR_PAD_LEFT);
-				$episode = str_pad($episode_number, 2, "0", STR_PAD_LEFT);
-				$episode_string = "S{$season}E{$episode}";
-				$file_path = "/mine/TVShows/{$show_info['show_title']}/Season {$season_number}/{$show_info['show_title']} - {$episode_string}";
-				if($now < $expire_ts) {
-					processUrl($attributes['permalink'], $file_path);
-				} else {
+				$entitlement = $attributes['entitlement'];
+				if($entitlement != "free") {
+					continue;
+				}
+
+				foreach($attributes['nbcAuthWindow'] as $window) {
+					if($window['type'] != "free") {
+						continue;
+					}
+					$end_ts = strtotime($window['end']);
+					if($now < $end_ts) {
+						$season_number = $attributes['seasonNumber'];
+						$episode_number = $attributes['episodeNumber'];
+						$season = str_pad($season_number, 2, "0", STR_PAD_LEFT);
+						$episode = str_pad($episode_number, 2, "0", STR_PAD_LEFT);
+						$episode_string = "S{$season}E{$episode}";
+						$file_path = "/mine/TVShows/{$show_info['show_title']}/Season {$season_number}/{$show_info['show_title']} - {$episode_string}";
+
+						processUrl($attributes['permalink'], $file_path);
+					}
 				}
 			}
+
+			unlink($data_file);
 		}
 	}
 }
