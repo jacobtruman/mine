197e992a6035d942abf3e041f93a01827f013578
Jacob Truman <jacob.truman@gmail.com>
2018-04-10 07:11:20 -0600

    update taillog

--- taillog.sh
+++ taillog.sh
@@ -1,5 +1,27 @@
-cd /raid/logs/
-grep '\[+\]' -A 20 TVShowFetch_$(date +%Y-%m-%d).log
+log_dir="/raid/logs"
+
+hashes="#############################################"
+log_file="$log_dir/TVShowFetch_$(date +%Y-%m-%d).log"
+log_file_yesterday="$log_dir/TVShowFetch_$(date +%Y-%m-%d --date='yesterday').log"
+echo $hashes
+echo "### $log_file_yesterday ###"
+echo $hashes
+cmd="grep \[+\] -A 10 $log_file_yesterday"
+echo $cmd
+$cmd
+
+echo $hashes
+echo "### $log_file ###"
+echo $hashes
+cmd="grep \[+\] -A 10 $log_file"
+echo $cmd
+$cmd
 #grep '\[-\]' -A 20 TVShowFetch_$(date +%Y-%m-%d).log
-tail TVShowFetch_$(date +%Y-%m-%d --date="yesterday").log
-tail -n 20 TVShowFetch_$(date +%Y-%m-%d).log
+
+cmd="tail $log_file_yesterday"
+echo $cmd
+$cmd
+
+cmd="tail -n 20 $log_file"
+echo $cmd
+$cmd
